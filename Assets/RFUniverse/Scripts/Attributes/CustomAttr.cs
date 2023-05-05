e4494faa896443b00152b86d34183c5555a5b2ee
yanbing.zhou <wrzh26252@outlook.com>
2023-05-05 19:21:08 +0800

    update custom attr

--- Assets/RFUniverse/Scripts/Attributes/CustomAttr.cs
+++ Assets/RFUniverse/Scripts/Attributes/CustomAttr.cs
@@ -2,62 +2,73 @@ using Robotflow.RFUniverse.SideChannels;
 using RFUniverse.Manager;
 using UnityEngine;
 using System.Linq;
+using Google.Protobuf.WellKnownTypes;
 
 namespace RFUniverse.Attributes
 {
+    /// <summary>
+    /// This is an example of custom attribute class, without actual functions.
+    /// </summary>
     public class CustomAttr : BaseAttr
     {
         public override void Init()
         {
             base.Init();
+            //Register the receiving function of the dynamic message
             AssetManager.Instance.AddListener("DynamicMessage", ListenerMessage);
         }
-        //数据发送示例
+
         public override void CollectData(OutgoingMessage msg)
         {
-            //先完成所继承的基类的数据写入
+            //1. First, complete the message parsing in parent class.
             base.CollectData(msg);
-            //按顺序写入数据
-            //此处写入顺序对应pyrfuniverse的custom_attr脚本parse_message函数中的读取顺序
-            msg.WriteString("this is a instance channel unity to python custom message");
+            //2. Write the message in order.
+            msg.WriteString("This is instance channel custom message");
         }
-        //新增接口示例
+
         public override void AnalysisMsg(IncomingMessage msg, string type)
         {
-            //根据头字符串分支
+            //1. Switch to implementation based on message type
             switch (type)
             {
                 case "CustomMessage":
                     CustomMessage(msg);
                     return;
             }
+            //2. The message analysis of the base class continues
             base.AnalysisMsg(msg, type);
         }
-        //接口实现
+
         void CustomMessage(IncomingMessage msg)
         {
-            //按顺序读取数据
-            //此处写入读取对应pyrfuniverse的custom_attr脚本CustomMessage函数中的写入顺序
+            //Read the message from python in order.
+            //Note that the reading order here should align with 
+            //the writing order in CustomMessage() of custom_attr.py.
             string str = msg.ReadString();
+
             Debug.Log(str);
         }
 
-        //ListenerMessage实现
         void ListenerMessage(IncomingMessage msg)
         {
+            //Read the message from python in order.
+            //Note that the reading order here should align with 
+            //the writing order in env.SendMessage() of test_custom_message.py.
             Debug.Log(msg.ReadInt32());
             Debug.Log(msg.ReadString());
             Debug.Log(msg.ReadBoolean());
             Debug.Log(msg.ReadFloat32());
             Debug.Log(msg.ReadFloatList());
 
+            //The SendMessage function can be called anywhere at any time
+            //Supported parameter types: string, int, float, bool, List<float>
             AssetManager.Instance.SendMessage(
                 "DynamicMessage",
-                "this is a unity to python dynamic message",
-                987654,
-                649849f,
+                "This is dynamic message",
+                123,
+                456f,
                 false,
-                (new float[] { 1321f, 989898f }).ToList()
+                (new float[] { 7.89f, 1.11f }).ToList()
                 );
         }
     }
--- Assets/RFUniverse/Scripts/Manager/AssetManagerExt.cs
+++ Assets/RFUniverse/Scripts/Manager/AssetManagerExt.cs
@@ -1,36 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Robotflow.RFUniverse.SideChannels;
-using RFUniverse.Manager;
+
 namespace RFUniverse.Manager
 {
     public class AssetManagerExt
     {
+        //You can extend AssetManager here
         public void AnalysisMsg(IncomingMessage msg, string type)
         {
             switch (type)
             {
-                //根据头字符串跳转不同实现函数
-                //此处CustomMessage对应pyrfuniverse的asset_channel_ext脚本中CustomMessage函数写入的第一个string
+                //Switch to implementation based on message type
                 case "CustomMessage":
                     CustomMessage(msg);
                     return;
             }
         }
-        //接口实现
+
         void CustomMessage(IncomingMessage msg)
         {
-            //根据pyrfuniverse的asset_channel_ext脚本中CustomMessage函数写入的参数读取
+            //Read the message from python in order.
+            //Note that the reading order here should align with 
+            //the writing order in env.CustomMessage() of asset_channel_ext.py.
             string str = msg.ReadString();
             Debug.Log(str);
-            //将接收到的参数发送到pyrfuniverse
+
+
+            //Send the received data back to python
+            //1. Define an out-going message
             OutgoingMessage sendMsg = new OutgoingMessage();
-            //第一个参数必须为头字符串
-            //此处CustomMessage对应pyrfuniverse的asset_channel_ext脚本中parse_message函数中一个分支
+            //2. The first data written must be the function name.
             sendMsg.WriteString("CustomMessage");
-            //按顺序写入数据
-            sendMsg.WriteString("this is a asset channel unity to python custom message");
+            //3. Write in the data.
+            sendMsg.WriteString("this is asset channel custom message");
             AssetManager.Instance.SendMessage(sendMsg);
         }
 
